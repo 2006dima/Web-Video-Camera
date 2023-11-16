42e97e89d7a9af9a158de5b8708ef56a60703ce1
2006dima <georgiandito@gmail.com>
2023-11-16 15:00:57 +0400

    Project done

---
+++ app.js
@@ -0,0 +1,27 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const snap = document.querySelector(".snap");
+const download = document.querySelector(".download");
+navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+  video.srcObject = stream;
+  video.play();
+});
+let image_data_url;
+let count = 0;
+snap.addEventListener("click", () => {
+  video.classList.toggle("effect");
+  setTimeout(() => video.classList.toggle("effect"), 300);
+  canvas
+    .getContext("2d")
+    .drawImage(video, 0, 0, canvas.width, canvas.height);
+  image_data_url = canvas.toDataURL("myPicture/png");
+  count++;
+});
+download.addEventListener("click", (e) => {
+  if (count === 0) {
+    e.preventDefault();
+    return alert("Please take photo before you can download");
+  }
+  download.href = image_data_url;
+  download.download = "myPicture";
+});
